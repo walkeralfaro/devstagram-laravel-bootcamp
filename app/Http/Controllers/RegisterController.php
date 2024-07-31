434c56ef3db47ca257b9c8831db56989e0f24724
walker <dev.walkeralfaro@gmail.com>
2024-07-31 00:33:45 -0500

    se agregaron migracion, vid 74

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 
 class RegisterController extends Controller
@@ -22,6 +24,13 @@ class RegisterController extends Controller
             'username' => 'required|string|max:50|min:2',
             'email' => 'required|string|email|max:60|unique:users',
             'password' => 'required|string|min:6|confirmed',
+        ]);
+
+        User::create([
+            'name' => $request->name,
+            'username' => $request->username,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
         ]);
     }
 }
